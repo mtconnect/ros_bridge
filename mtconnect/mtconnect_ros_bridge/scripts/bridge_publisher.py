45b94b6c3612d2914b8fcff7a75c78c85540bf90
Stephen Wiedmann <swiedmann@swri.org>
2013-01-22 11:02:16 -0600

    changed bridge publisher to handle 1-n topics. updated config file to include url.

--- mtconnect/mtconnect_ros_bridge/scripts/bridge_publisher.py
+++ mtconnect/mtconnect_ros_bridge/scripts/bridge_publisher.py
@@ -45,74 +45,43 @@ class BridgePublisher():
                 
         # Setup MTConnect to ROS Conversion
         self.config = read_config_file.obtain_dataMap()
-        #rospy.loginfo('config file --> %s' % self.config.keys())
         
-        # Create the data sets for the topic types, member names, and member types
+        # Create the data lists for the topic names, types, manifests, data items, ROS publishers, and ROS messages
+        self.topic_name_list = []
+        self.topic_type_list = []
+        self.lib_manifests = []
+        self.data_items = []
+        self.pub = []
+        self.msg = []
+        
+        # Setup topic, data items, pub, etc. via configuration file
         self.setup_topic_data()
         
-        """
-        # Setup ROS publisher and topic hash table keys
-        for topic, topic_type in self.config.items():
-            # TOPIC --> topic name, such as 'chatter'
-            # TOPIC TYPE --> message namespace and name, such as mtconnect_msgs/CncStatus
-            tokens = topic_type.keys()[0].split('/')
-            namespace = tokens[0]
-            type_name = tokens[1]
-            
-            # Import module and create topic type class, called pub_handle
-            #    i.e. append <class 'mtconnect_msgs.msg._RobotStates.RobotStates'>
-            rospy.loginfo('Class Instance --> ' + namespace + '.msg.' + type_name)
-            
-            roslib.load_manifest(namespace)
-            
-            pub_handle = getattr(import_module(namespace + '.msg'), type_name)
-            
-            # Create ROS publisher
-            self.pub = rospy.Publisher(topic, pub_handle)
-            
-            # Instance of topic type message class
-            self.msg = pub_handle()
-            
-            # Capture topic name and type for dataMap reference
-            self.topic_name = topic
-            self.topic_type = topic_type.keys()[0] # Only one type per topic
-            
-            # Store the data items for this topic
-            self.data_items = [data_item for data_item in topic_type[self.topic_type].keys()]
-        """     
         # Hash table of persistent CNC event states, only updates when state changes
         self.di_current = {}
         
         # Hash table to store changed CNC event states
         self.di_changed = None
         
-        # With the CNC localhost connection, read in current CNC XML
-        conn = HTTPConnection('localhost', 5000)
-        conn.request("GET", "/cnc/current")
-        response = conn.getresponse()
-        if response.status != 200:
-            rospy.loginfo("Request failed: %s - %d" % (response.reason, response.status))
-            sys.exit(0)
+        # Establish XML connection, read in current XML
+        conn = HTTPConnection('localhost', self.config['localhost'])
+        response = self.xml_http_connection(conn, "/cnc/current")
         cnc_body = response.read()
         
-        # Parse the XML and determine the current sequence
+        # Parse the XML and determine the current sequence and XML Event elements
         seq, elements = self.xml_components(cnc_body)
         
-        # Use XML to establish CNC state dictionary
+        # Use XML to establish current data item state dictionary
         self.di_current = {e.attrib['name']:e.text for e in elements if 'name' in e.attrib.keys()}
         
         # Confirm that data items in config file are present in XML
-        if not set(self.data_items).issubset(set(self.di_current.keys())):
-            rospy.logerr('ERROR: INCORRECT EVENTS IN TOPIC CONFIG OR XML AGENT FILE')
-            sys.exit()
+        for di_set in self.data_items:
+            if not set(di_set).issubset(set(self.di_current.keys())):
+                rospy.logerr('ERROR: INCORRECT EVENTS IN TOPIC CONFIG OR XML AGENT FILE')
+                sys.exit()
         
         # Start a streaming XML connection
-        conn.request("GET", "/cnc/sample?interval=1000&count=1000&from=" + seq)
-        response = conn.getresponse()
-        #---------------------------DEBUG------------------
-        #for key, val in self.di_current.items():
-        #    rospy.loginfo('CNC KEY --> %s\tVAL --> %s' % (key, val))
-        #---------------------------DEBUG------------------
+        response = self.xml_http_connection(conn, "/cnc/sample?interval=1000&count=1000&from=" + seq)
         
         # Create class lock
         self.lock = thread.allocate_lock()
@@ -133,47 +102,58 @@ class BridgePublisher():
         via the getattr(import_module) function.  Data is stored in the
         following class attributes:
         
-            self.topic_type_list   --> used for module import and msg parameters
-            self.member_types --> member type, not used in msg parameters, future use
-            self.member_names --> used for ROS subscriber msg parameters
+            self.data_items   --> used for ROS data structure tracking
+            self.pub          --> used to setup ROS topic publishers
+            self.msg          --> data structure for msg class instances of the topic type
+            self.topic_name_list  --> used for configuration file key reference
+            self.topic_type_list  --> used for module import and config file key reference
         """
+        
+        for topic_name, type_name in self.config.items():
+            if topic_name != 'localhost':
+                # TOPIC --> topic name, such as 'chatter'
+                # TOPIC TYPE --> message namespace and name, such as mtconnect_msgs/CncStatus
+                tn = type_name.keys()[0]
+                tokens = tn.split('/')
+                namespace = tokens[0]
+                topic_type_name = tokens[1]
+                
+                # Load package manifest if unique
+                if tokens[0] not in self.lib_manifests:
+                    roslib.load_manifest(tokens[0])
+                    self.lib_manifests.append(tokens[0])
+    
+                # Import module and create topic type class,
+                #    i.e. append <class 'mtconnect_msgs.msg._RobotStates.RobotStates'>
+                rospy.loginfo('Class Instance --> ' + namespace + '.msg.' + topic_type_name)
+                type_handle = getattr(import_module(namespace + '.msg'), topic_type_name)
+                
+                # Append list of data items specified in the ROS topic message
+                self.data_items.append([val for val in type_handle.__slots__ if val != 'header'])
+                
+                # Create ROS publisher
+                self.pub.append(rospy.Publisher(topic_name, type_handle))
+                
+                # Instance of topic type message class
+                self.msg.append(type_handle())
+                
+                # Capture topic name and type for self.config reference
+                self.topic_name_list.append(topic_name)
+                self.topic_type_list.append(type_name.keys()[0]) # Only one type per topic
+        
         rospy.loginfo('TOPIC NAME LIST --> %s' % self.topic_name_list)
         
-        for topic_name, type_name in zip(self.topic_name_list, self.subscribed_list):
-            # TOPIC --> topic name, such as 'chatter'
-            # TOPIC TYPE --> message namespace and name, such as mtconnect_msgs/CncStatus
-            tokens = type_name.split('/')
-            namespace = tokens[0]
-            type_name = tokens[1]
-            
-            # Load package manifest if unique
-            if tokens[0] not in self.lib_manifests:
-                roslib.load_manifest(tokens[0])
-                self.lib_manifests.append(tokens[0])
-
-            # Import module and create topic type class,
-            #    i.e. append <class 'mtconnect_msgs.msg._RobotStates.RobotStates'>
-            rospy.loginfo('Class Instance --> ' + namespace + '.msg.' + type_name)
-            type_handle = getattr(import_module(namespace + '.msg'), type_name)
-
-            #self.topic_type_list.append(type_handle)
-            #self.msg_text.append(type_handle._full_text)
-            #self.member_types.append(type_handle._slot_types)
-            #self.member_names.append(type_handle.__slots__)
-            self.data_items.append([val for val in type_handle.__slots__ if val != 'header'])
-            
-            # Create ROS publisher
-            self.pub.append(rospy.Publisher(topic_name, type_handle))
-            
-            # Instance of topic type message class
-            self.msg.append(type_handle())
-            
-            # Capture topic name and type for dataMap reference
-            self.topic_name = topic_name
-            self.topic_type = topic_type.keys()[0] # Only one type per topic
-            
-            
         return
+
+    def xml_http_connection(self, conn, req):
+        conn.request("GET", req)
+        response = conn.getresponse()
+        if response.status != 200:
+            rospy.loginfo("Request failed: %s - %d" % (response.reason, response.status))
+            sys.exit(0)
+        else:
+            rospy.loginfo('Request --> %s, Status --> %s' % (response.reason, response.status))
+        return response
 
     def xml_components(self, xml):
         """ Find all elements in the updated xml.
@@ -189,7 +169,10 @@ class BridgePublisher():
         return nextSeq, elements
     
     def init_di_dict(self):
-        return {item:None for item in self.data_items}
+        """ Initialize the data item dictionary with data item : None
+        pairs.  Dictionary used to record only changed event states.
+        """
+        return {val:None for item in self.data_items for val in item}
     
     def process_xml(self, xml):
         """Function determines if an event of interest has changed and updates
@@ -207,52 +190,59 @@ class BridgePublisher():
         for e in elements:
             rospy.loginfo('Changing ROS message --> %s --> %s' % (e.tag, e.text))
             
+            di_list = [val for di_vals in self.data_items for val in di_vals]
+
             if 'name' in e.attrib.keys():
-                if e.attrib['name'] in self.data_items:
+                if e.attrib['name'] in di_list:
                     # Change local_di from None to current MTConnect event status
                     di = e.attrib['name']
                     local_di[di] = e.text
                     self.di_current[di] = e.text # Update di_current with new state
         return nextSeq, local_di
 
-    def topic_publisher(self):
+    def topic_publisher(self, cb_data):
         """ Publishes the MTConnect message.  This function is a generic publisher.
         Attribute fetches are obtained via the attrgetter function in the operator
         module.  In order to assign a value to a relative attribute, a code object
         is created and then executed.  
         """
-        self.msg.header.stamp = rospy.Time.now()
+        topic_name, topic_type, pub, msg, data_items = cb_data
+        
+        msg.header.stamp = rospy.Time.now()
         
         for di_name, value in self.di_changed.items():
             # Convert tag text from MTConnect to ROS
-            if value is not None:
-                # Used changed value in di_changed
-                rospy.loginfo('%s changed, set from di_changed' % di_name) # DEBUG
+            if di_name in data_items:
+                if value is not None:
+                    # Used changed value in di_changed
+                    rospy.loginfo('%s changed, set from di_changed' % di_name) # DEBUG
+                    
+                    # Check for conversion key error
+                    conv_keys = self.config[topic_name][topic_type][di_name].keys()
+                    if value not in conv_keys:
+                        rospy.logerr("CONVERSION ERROR IN TOPIC CONFIG FILE: XML is '%s' --> CONFIG is %s" % (value, conv_keys))
+                        os._exit(0)
+                    
+                    # Convert from MTConnect to ROS format from di_changed
+                    ros_tag = self.config[topic_name][topic_type][di_name][self.di_changed[di_name]]
+                else:
+                    # Use stored value in di_current
+                    #rospy.loginfo('%s did not change, set from di_current' % di_name) # DEBUG
+                    
+                    # Convert from MTConnect to ROS format via di_current
+                    ros_tag = self.config[topic_name][topic_type][di_name][self.di_current[di_name]]
                 
-                # Check for conversion key error
-                conv_keys = self.config[self.topic_name][self.topic_type][di_name].keys()
-                if value not in conv_keys:
-                    rospy.logerr("CONVERSION ERROR IN TOPIC CONFIG FILE: XML is '%s' --> CONFIG is %s" % (value, conv_keys))
-                    os._exit(0)
+                #rospy.loginfo('%s, %s' % (di_name, value))
                 
-                # Convert from MTConnect to ROS format from di_changed
-                ros_tag = self.config[self.topic_name][self.topic_type][di_name][self.di_changed[di_name]]
-            else:
-                # Use stored value in di_current
-                #rospy.loginfo('%s did not change, set from di_current' % di_name) # DEBUG
-                
-                # Convert from MTConnect to ROS format via di_current
-                ros_tag = self.config[self.topic_name][self.topic_type][di_name][self.di_current[di_name]]
+                # Obtain the state value via callable object: 'door_state.CLOSED'
+                valueCall = operator.attrgetter(di_name + '.' + ros_tag)
+                state_value = valueCall(msg)
+    
+                # Create a code object and execute to assign the attribute to the state value
+                co_str = compile('msg.' + di_name + '.val = state_value', '', 'exec')
+                exec(co_str)
 
-            # Obtain the state value via callable object: 'door_state.CLOSED'
-            valueCall = operator.attrgetter(di_name + '.' + ros_tag)
-            state_value = valueCall(self.msg)
-
-            # Create a code object and execute to assign the attribute to the state value
-            co_str = compile('self.msg.' + di_name + '.val = state_value', '', 'exec')
-            exec(co_str)
-
-        self.pub.publish(self.msg)
+        pub.publish(msg)
         return
 
     def xml_callback(self, chunk):
@@ -261,7 +251,7 @@ class BridgePublisher():
         try:
             _, self.di_changed = self.process_xml(chunk)
         except Exception as e:
-            rospy.logerr("Rostopic relay process XML callback failed: %s, releasing lock" % e)
+            rospy.logerr("Bridge Publisher: Process XML callback failed: %s, releasing lock" % e)
         finally:
             self.lock.release()
         #rospy.loginfo('*******************Done with PROCESS_XML callback***************')
@@ -272,13 +262,17 @@ class BridgePublisher():
         self.lock.acquire()
         try:
             if self.di_changed != None:
-                # Publish topic
-                self.topic_publisher()
+                publishers = []
+                for topic_name, topic_type, pub, message, di in zip(self.topic_name_list, self.topic_type_list,
+                                                                self.pub, self.msg, self.data_items):
+                    # Publish topic
+                    rospy.init_node('bridge_publisher')
+                    publishers.append(self.topic_publisher((topic_name, topic_type, pub, message, di)))
                 
                 # Reset current data items to None
                 self.di_changed = self.init_di_dict()
         except Exception as e:
-            rospy.logerr("Rostopic relay publisher callback failed: %s, releasing lock" % e)        
+            rospy.logerr("Bridge Publisher: ROS-Publisher callback failed: %s, releasing lock" % e)        
         finally:
             self.lock.release()
         #rospy.loginfo('-------------------Done with ROS_PUBLISHER callback---------------')
