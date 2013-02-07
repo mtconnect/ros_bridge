8d999bb72365006c069d2a4643f054b837ececb2
Stephen Wiedmann <swiedmann@swri.org>
2013-02-07 15:52:50 -0600

    updated bridge actions and messages.  updated clients with XML queueing

--- mtconnect/mtconnect_ros_bridge/scripts/bridge_server.py
+++ mtconnect/mtconnect_ros_bridge/scripts/bridge_server.py
@@ -23,6 +23,8 @@ import operator
 import thread
 import re
 import time
+from Queue import Queue
+from threading import Thread, Timer
 from importlib import import_module
 from httplib import HTTPConnection
 from xml.etree import ElementTree
@@ -49,10 +51,15 @@ class GenericActionServer():
         
         # Setup MTConnect to ROS Conversion
         self.config = read_config_file.obtain_dataMap()
+        self.msg_parameters = ['url', 'url_port', 'machine_tool', 'xml_namespace', 'adapter_port']
+        self.url = self.config[self.msg_parameters[0]]
+        self.url_port = self.config[self.msg_parameters[1]]
+        self.mtool = self.config[self.msg_parameters[2]]
+        self.ns = dict(m = self.config[self.msg_parameters[3]])
+        self.port = self.config[self.msg_parameters[4]]
         
         # Setup MTConnect Adapter for robot status data items
-        self.adapter = Adapter(('0.0.0.0', self.config['Port']))
-        self.ns = dict(m = 'urn:mtconnect.org:MTConnectStreams:1.2')
+        self.adapter = Adapter((self.url, self.port))
         
         # Create empty lists for actions, message type handles, etc.
         self.lib_manifests = []
@@ -60,9 +67,17 @@ class GenericActionServer():
         self.action_list = {}
         self.action_goals = {}
         self.action_conv = []
-        self.namespace = None
-        self.xml_goal = None
+        self.capture_xml = False
         self.di_dict = {} # XML data item dictionary to store MTConnect Adapter Events
+        
+        # Create a dictionary of _results to return upon action success
+        self._resultDict = {}
+        
+        # Create a dictionary of action servers
+        self._as = {}
+        
+        # Create a dictionary of server names
+        self.server_name = {}
         
         # Setup action client data per the config file
         self.setup_topic_data()
@@ -75,53 +90,25 @@ class GenericActionServer():
         self.adapter.start()
         
         # Establish XML connection, read in current XML
-        self.conn = HTTPConnection('localhost', self.config['localhost'])
-        #response = self.xml_get_response("/cnc/current")
-        #cnc_body = response.read()
+        self.conn = HTTPConnection(self.url, self.url_port)
+        response = self.xml_get_response(self.mtool + "/current")
+        body = response.read()
         
         # Parse the XML and determine the current sequence and XML Event elements
-        #seq, elements = self.xml_components(cnc_body)
+        seq, elements = self.xml_components(body)
         
         # Start a streaming XML connection
-        #response = self.xml_get_response("/cnc/sample?interval=1000&count=1000&from=" + seq)
+        response = self.xml_get_response(self.mtool + "/sample?interval=1000&count=1000&from=" + seq)
         
-        # Create class lock
-        #self.lock = thread.allocate_lock()
-
+        # Create queue for streaming XML
+        self.XML_queue = Queue()
+        
         # Create XML polling thread
-        #lp = LongPull(response)
-        #lp.long_pull(self.xml_callback) # Runs until user interrupts
+        lp = LongPull(response)
+        xml_thread = Thread(target = lp.long_pull, args = (self.xml_callback,))
+        xml_thread.daemon = True
+        xml_thread.start()
         
-        # Create a dictionary of _results to return upon action success
-        self._resultDict = {}
-        
-        # Create a dictionary of action servers
-        self._as = {}
-        
-        # Create a dictionary of server names
-        self.server_name = {}
-        
-        # Iterate through actions and create action call-backs and action result class instances
-        for ns, action in self.config.items():
-            if ns != 'localhost' and ns != 'Port': # Only one ROS namespace in config by design
-                for request in action.keys():
-                    di_conv = self.split_event(request)
-                    self.server_name[di_conv] = request
-                    
-                    # Create a dictionary of result class instances
-                    request_class = getattr(self.type_handle, request + 'Result')
-                    self._resultDict[request] = request_class()
-                    
-                    # Create instance of the action class
-                    action_class = getattr(self.type_handle, request + 'Action')
-                    
-                    # Create action server for requested action
-                    self._as[request] = actionlib.SimpleActionServer(request + 'Client', action_class, self.execute_cb, False)
-                    
-                    # Start the action server
-                    self._as[request].start()
-        
-    
     def add_agent(self):
         # For each data item in config file, add Event and data item
         for xml_tag in self.action_list:
@@ -162,7 +149,7 @@ class GenericActionServer():
         """
         
         for namespace, action in self.config.items():
-            if namespace != 'localhost' and namespace != 'Port': # Only one ROS namespace in config by design
+            if namespace not in self.msg_parameters: # Only one ROS namespace in config by design
                 # Load package manifest if unique
                 if namespace not in self.lib_manifests:
                     roslib.load_manifest(namespace)
@@ -172,19 +159,34 @@ class GenericActionServer():
                 rospy.loginfo('Importing --> ' + namespace + '.msg')
                 self.type_handle = import_module(namespace + '.msg')
                 
-                # Capture namespace for action client
-                self.namespace = namespace
-                
-                for action_req in action.keys():
+                # Iterate through requests and create action call-backs and action result class instances
+                for request in action.keys():
                     # Capture action name for action client callback reference
-                    self.action_list[action_req] = action_req
+                    self.action_list[request] = request
                     
                     # Capture ROS Action parameters
-                    goal_key = self.config[namespace][action_req].keys()[0]
-                    self.action_goals[action_req] = self.config[namespace][action_req][goal_key].keys()
+                    goal_key = self.config[namespace][request].keys()[0]
+                    self.action_goals[request] = self.config[namespace][request][goal_key].keys()
                     
                     # Capture ROS to MTConnect state conversion dictionary
-                    #self.action_conv[action_req] = self.config[namespace][action_req]['conversion']
+                    #self.action_conv[request] = self.config[namespace][request]['conversion']
+                    
+                    # Store the ROS server name in a hash table
+                    di_conv = self.split_event(request)
+                    self.server_name[di_conv] = request
+                    
+                    # Create a dictionary of result class instances
+                    request_class = getattr(self.type_handle, request + 'Result')
+                    self._resultDict[request] = request_class()
+                    
+                    # Create instance of the action class
+                    action_class = getattr(self.type_handle, request + 'Action')
+                    
+                    # Create action server for requested action, store in a hash table
+                    self._as[request] = actionlib.SimpleActionServer(request + 'Client', action_class, self.execute_cb, False)
+                    
+                    # Start the action server
+                    self._as[request].start()
         return
     
     def xml_get_response(self, req):
@@ -198,8 +200,7 @@ class GenericActionServer():
         return response
     
     def xml_components(self, xml):
-        """ Find all elements in the updated xml.
-        root.find requires namespaces to be a dictionary.
+        """ Find all elements in the updated xml.  root.find requires namespaces to be a dictionary.
         Return sequence and elements to process ROS msgs.
         """
         root = ElementTree.fromstring(xml)
@@ -208,7 +209,7 @@ class GenericActionServer():
        
         elements = []
 
-        for action in self.action_list:
+        for action in self.action_list.keys():
             find_action = root.findall('.//m:' + action, namespaces=self.ns)
             if find_action: # Element list is not empty
                 elements.append(find_action[0])
@@ -217,6 +218,7 @@ class GenericActionServer():
     def execute_cb(self, goal):
         # If goal is OpenDoor, use the goal defined in the message as your key, access it as goal.__slots__[0]
         action = goal.__slots__[0]
+        
         # Empty function -- assumes action was successful
         rospy.loginfo('In %s Callback -- determining action request result.' % self.server_name[action])
         
@@ -227,36 +229,38 @@ class GenericActionServer():
         self.adapter.complete_gather()
         robot_hold = 0 # Used to make sure you only change robot state once after 'COMPLETE' received from CNC
         
-        # While loop to poll CNC XML until READY is received for OpenDoor
+        # Start capturing XML
+        self.capture_xml = True
+        
+        # While loop to poll CNC XML until READY is received for Robot action request
         dwell = True
-        while dwell == True: # Replace text with dynamic reference to yaml file
-            # While polling monitor CNC response for ACTIVE,
-            # if so, inform Action Client that goal has been accepted -- use 'set accepted' method
-            # remain in while loop
+        while dwell == True:
+            # Obtain XML chunk
+            if not self.XML_queue.empty():
+                chunk = self.XML_queue.get()
+                
+                # Parse the XML and determine the current sequence and XML Event elements
+                root = ElementTree.fromstring(chunk)
+                element = root.findall('.//m:' + self.server_name[action], namespaces = self.ns)[0]
             
-            # Poll CNC XML for goal state
-            response = self.xml_get_response("/cnc/current")
-            cnc_body = response.read()
-            
-            # Parse the XML and determine the current sequence and XML Event elements
-            root = ElementTree.fromstring(cnc_body)
-            element = root.findall('.//m:' + self.server_name[action], namespaces=self.ns)[0]
-            
-            if element.text == 'ACTIVE':
-                # Set accepted back to action client
-                pass
-            
-            # While polling monitor CNC response for COMPLETE, submit READY handshake
-            elif element.text == 'COMPLETE' and robot_hold == 0: # Need to check robot XML as well
-                rospy.loginfo("Changing %s to 'READY'" % self.server_name[action])
-                self.adapter.begin_gather()
-                self.di_dict[action].set_value('READY')
-                self.adapter.complete_gather()
-                robot_hold = 1
-            
-            elif element.text == 'READY' and robot_hold == 1:
-                dwell = False
-            
+                if element.text == 'ACTIVE':
+                    # Set accepted back to action client
+                    pass
+                
+                # While polling monitor CNC response for COMPLETE, submit READY handshake
+                elif element.text == 'COMPLETE' and robot_hold == 0: # Need to check robot XML as well
+                    rospy.loginfo("Changing %s to 'READY'" % self.server_name[action])
+                    self.adapter.begin_gather()
+                    self.di_dict[action].set_value('READY')
+                    self.adapter.complete_gather()
+                    robot_hold = 1
+                
+                elif element.text == 'READY' and robot_hold == 1:
+                    dwell = False
+                    self.capture_xml = False
+                
+                # Release the queue
+                self.XML_queue.task_done()
             
         # When response is READY, set server result and communicate as below:
          
@@ -264,12 +268,26 @@ class GenericActionServer():
         result_attribute = self._resultDict[self.server_name[action]].__slots__[0]
         
         # Create code object to set the attribute per the ROS to MTConnect conversion
-        #setattr(self._resultDict[self.server_name[action]], result_attribute, self.config[self.server_name[action]][tbd])
         setattr(self._resultDict[self.server_name[action]], result_attribute, 'READY')
         
         # Indicate a successful action
         self._as[self.server_name[action]].set_succeeded(self._resultDict[self.server_name[action]])
         rospy.loginfo('In %s Callback -- action succeeded.' % self.server_name[action])
+        return
+    
+    def xml_callback(self, chunk):
+        #rospy.loginfo('*******************In PROCESS_XML callback***************')
+        try:
+            if self.capture_xml == True:
+                self.XML_queue.put(chunk)
+                #rospy.loginfo('PUTTING XML INTO QUEUE %s\tNUMBER OF QUEUED OBJECTS %s' % (self.XML_queue, self.XML_queue.qsize()))
+                if self.XML_queue.qsize() > 1:
+                    rospy.loginfo('STORED XML INTO QUEUE, WAITING ON ROS ACTION SERVER, QUEUE SIZE --> %s' % self.XML_queue.qsize())
+        except Exception as e:
+            rospy.logerr("Bridge Server: Process XML callback failed: %s, releasing lock" % e)
+        finally:
+            pass
+        #rospy.loginfo('*******************Done with PROCESS_XML callback***************')
         return
 
 if __name__ == '__main__':
--- mtconnect/mtconnect_ros_bridge/scripts/material_load_server.py
+++ mtconnect/mtconnect_ros_bridge/scripts/material_load_server.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 """
    Copyright 2013 Southwest Research Institute
