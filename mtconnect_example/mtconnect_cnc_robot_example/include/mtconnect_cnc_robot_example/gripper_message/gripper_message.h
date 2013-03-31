c4668475fc450a885f74baf5e93036b4662297d9
jrgnicho <jrgnicho@gmail.com>
2013-03-31 15:09:49 -0500

    added source and header files for grasp action server interface and test utility

---
+++ mtconnect_example/mtconnect_cnc_robot_example/include/mtconnect_cnc_robot_example/gripper_message/gripper_message.h
@@ -0,0 +1,139 @@
+﻿/*
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2011, Southwest Research Institute
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 	* Redistributions of source code must retain the above copyright
+ * 	notice, this list of conditions and the following disclaimer.
+ * 	* Redistributions in binary form must reproduce the above copyright
+ * 	notice, this list of conditions and the following disclaimer in the
+ * 	documentation and/or other materials provided with the distribution.
+ * 	* Neither the name of the Southwest Research Institute, nor the names
+ *	of its contributors may be used to endorse or promote products derived
+ *	from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GRIPPER_MESSAGE_H
+#define GRIPPER_MESSAGE_H
+
+#ifdef ROS
+#include "simple_message/typed_message.h"
+#include "simple_message/simple_message.h"
+#include "simple_message/shared_types.h"
+//#include "longhorn/longhorn.h"
+#endif
+
+#ifdef MOTOPLUS
+#include "typed_message.h"
+#include "simple_message.h"
+#include "shared_types.h"
+//#include "longhorn.h"
+#endif
+
+
+namespace mtconnect_cnc_robot_example
+{
+namespace gripper_message
+{
+
+/**
+ * \brief Enumeration of gripper operations
+ */
+namespace GripperOperationTypes
+{
+  enum GripperOperationType
+  {
+    INVALID = 0,
+    INIT,
+    OPEN,
+    CLOSE
+  };
+}
+typedef GripperOperationTypes::GripperOperationType GripperOperationType;
+
+/**
+ * \brief Class encapsulated longhorn robot gripper message.
+ */
+//* GripperMessage
+/**
+ *
+ *
+ * THIS CLASS IS NOT THREAD-SAFE
+ *
+ */
+
+class GripperMessage : public industrial::typed_message::TypedMessage
+
+{
+public:
+  /**
+   * \brief Default constructor
+   *
+   * This method creates an empty message.
+   *
+   */
+  GripperMessage(void);
+  /**
+   * \brief Destructor
+   *
+   */
+  ~GripperMessage(void);
+  /**
+   * \brief Initializes message from a simple message
+   *
+   * \param simple message to construct from
+   *
+   * \return true if message successfully initialized, otherwise false
+   */
+  bool init(industrial::simple_message::SimpleMessage & msg);
+
+  /**
+   * \brief Initializes message from a joint trajectory point structure
+   *
+   * \param gripper operation enumeration
+   *
+   */
+  void init(GripperOperationType operation);
+
+  /**
+   * \brief Initializes a new message
+   *
+   */
+  void init();
+
+  // Overrides - SimpleSerialize
+  bool load(industrial::byte_array::ByteArray *buffer);
+  bool unload(industrial::byte_array::ByteArray *buffer);
+
+  unsigned int byteLength()
+  {
+    return sizeof(industrial::shared_types::shared_int(operation_));
+  }
+
+  GripperOperationType operation_;
+
+private:
+
+
+};
+
+}
+}
+
+#endif /* GRIPPER_MESSAGE_H */
