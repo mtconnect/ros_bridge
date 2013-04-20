cb34fddd9b5c4c32d33dfd10c821eec2daa48a7a
Will Sobel <will@systeminsights.com>
2013-04-20 00:29:12 -0700

    Moved source files into src directory to clean up sim root.

--- simulator/cnc_simulator.rb
+++ simulator/cnc_simulator.rb
@@ -13,6 +13,7 @@
 #    limitations under the License.
 
 $: << '.'
+$: << './src'
 
 require 'cnc'
 require 'streamer'
--- simulator/spec/spec_helper.rb
+++ simulator/spec/spec_helper.rb
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-$: << File.dirname(__FILE__) + '/..'
+$: << File.dirname(__FILE__) + '/../src'
 
 module Helpers
   def unloading
